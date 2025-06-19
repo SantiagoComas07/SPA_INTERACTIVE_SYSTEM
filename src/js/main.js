16e40762f85e1bb699fab4377d5420e7acde4fae
SantiagoComas07 <santiagocomas07d@gmail.com>
2025-06-19 08:09:15 -0500

    feat: new changes, the sistem has new answer depending fo the values

--- src/js/main.js
+++ src/js/main.js
@@ -8,8 +8,13 @@ const box_datas = document.getElementById("sub-box")
 // const box = document.getElementById("consult")
 const btn_back = document.getElementById("back")
 
-let message
+const resultCourseA=document.getElementById("result_A")
+const resultCourseB=document.getElementById("result_B")
+const resultCourseC=document.getElementById("result_C")
 
+
+let message
+let flag
 
 function identificar(name, age){    
 
@@ -21,34 +26,42 @@ if (isNaN(age)){
 
 }else if(age >=18){
     message=`Hola ${name}, eres mayor de edad. ¡Preparate para las grandes oportunidades en programación!`
-
+    flag=true
 }else{
     message=`Hola ${name}, eres menor de edad. ¡Sigue aprendiendo y disfrutando del codigo!`
+    flag=false
 }}
 
 
-function showResultCourses(){
-
+function showResultCourses(ageUser,answerA,answerB,answerC, confirm){
+    ageUser= parseInt(ageUser);
+    if(isNaN(ageUser)){
+      //Sin funciones por el momento
+    }else{
+        if(confirm){
+            answerA.textContent='Desarrollo web avanzado'
+            answerB.textContent='Desarrollo web avanzado'
+            answerC.textContent='Desarrollo web avanzado'
+        }else{
+            answerA.textContent='Logica de programación'
+            answerB.textContent='Logica de programación'
+            answerC.textContent='Logica de programación'
+    
+        }
+    }
 
 
 }
 
-
-
+// This code line is to ocult the box of consult
 box_consult_back.style.display="none"
-
-
-
-
-
-
-
 
 btn.addEventListener('click', ()=>{
     identificar(nameUser.value, ageUser.value)
     result.textContent= message
     box_consult.style.display="none"
     box_consult_back.style.display="block"
+    showResultCourses( resultCourseA, resultCourseB, resultCourseB, flag)
 })
 
 btn_back.addEventListener('click', ()=>{
