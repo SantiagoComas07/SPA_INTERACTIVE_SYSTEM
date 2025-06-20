ee6bfb7b5b10cf243715c3a1720a9d10f42c179e
Santiago Andrés Comas Durán <santiagocomas07d@gmail.com>
2025-06-20 18:18:45 -0500

    feat: new change (responsive v1.0, virtual asistant)

--- src/js/main.js
+++ src/js/main.js
@@ -19,6 +19,7 @@ let answerB
 let answerC
 let mainFlag
 let flag
+let real = true;
 
 
 function identificar(name, age){    
@@ -29,20 +30,29 @@ if (isNaN(age)){
     console.error("Error...Informacion invalida");
     message= `Hola ${name} su información es invalida`
     mainFlag=true
-
-}else if(age >=18){
+// The student must be of a real age, so there is a condition that saves that age.
+}else if(age >=18 && age<=100){
     message=`Hello ${name}, You're of legal age. Take advantage of great opportunities with our professional courses!`
     flag=true
-}else{
+}else if(age>5 && age<18){
     message=`Hello ${name}, You're a minor. Start with the basics!`
     flag=false
+}else{
+message=`You don't real`
+real= false;
 }}
 
 
-function showResultCourses(palabra, palabraA){
+function showResultCourses(variable, consfirmation, confirmationTwo){
     if(mainFlag==true){
       console.error("Insufficient information")
     }else{
+        if(!real){
+            console.warn("The age is false")
+            answerA='xxxxx'
+            answerB='xxxxx'
+            answerC='xxxxx'
+        }else{
         if(flag==true){
             console.warn("TODO EN ORDEN")
             answerA='React Developer Essentials'
@@ -53,7 +63,8 @@ function showResultCourses(palabra, palabraA){
             answerB='Smart Web Navigation: Master the Internet Safely and Efficiently'
             answerC='Python Programming: From Zero to Backend Developer'
 
-        }
+        }}
+
 
 
     }
@@ -75,7 +86,8 @@ btn.addEventListener('click', ()=>{
         }
     box_consult.style.display="none"
     box_consult_back.style.display="block"
-    showResultCourses( userAge, flag)
+    showResultCourses( userAge, flag, real)
+    real = true
     resultCourseA.textContent=answerA
     resultCourseB.textContent=answerB
     resultCourseC.textContent=answerC
